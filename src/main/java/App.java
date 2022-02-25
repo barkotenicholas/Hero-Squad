efb0d68d686ef1a73b6a080864563a123dfe1094
barkotenicholas <barkotenicholas@gmail.com>
2022-02-25 20:41:53 +0300

    Added forms

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,16 +3,17 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
 
 public class App {
 
     public static void main(String[] args) {
 
-        get("/",(request, response) -> {
-                return new ModelAndView(new HashMap<>(),"index.hbs");
-        },new HandlebarsTemplateEngine());
+        get("/",(request, response) -> new ModelAndView(new HashMap<>(),"index.hbs"),new HandlebarsTemplateEngine());
 
+        get("/squad/new",(request, response) -> {
+            return new ModelAndView(new HashMap<>(),"squad.hbs");
+        },new HandlebarsTemplateEngine());
 
     }
 }
