dc0ebab4f484df4d4983f7a8d1096312e1d022fc
barkotenicholas <barkotenicholas@gmail.com>
2022-02-25 20:12:04 +0300

    Added index.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,18 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.*;
+
 public class App {
+
     public static void main(String[] args) {
+
+        get("/",(request, response) -> {
+                return new ModelAndView(new HashMap<>(),"index.hbs");
+        },new HandlebarsTemplateEngine());
+
 
     }
 }
