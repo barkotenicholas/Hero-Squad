9d937ed1fcffd267cedf3040f15092b06521de07
barkotenicholas <barkotenicholas@gmail.com>
2022-02-25 21:34:54 +0300

    Added content in success.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,12 @@
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class App {
 
@@ -12,8 +15,22 @@ public class App {
         get("/",(request, response) -> new ModelAndView(new HashMap<>(),"index.hbs"),new HandlebarsTemplateEngine());
 
         get("/squad/new",(request, response) -> {
+
             return new ModelAndView(new HashMap<>(),"squad.hbs");
         },new HandlebarsTemplateEngine());
 
+        post("/squad",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            String squadName = request.queryParams("squadName");
+            String squadPurpose = request.queryParams("squadPurpose");
+            int squadSize = Integer.parseInt(request.queryParams("size"));
+
+            Squad squad = new Squad(squadName,squadSize,squadPurpose);
+
+            model.put("squadName",squadName);
+
+            return new ModelAndView(model,"success.hbs");
+        },new HandlebarsTemplateEngine());
     }
 }
