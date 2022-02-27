61f7d8801a53e1f72b47aebdf87afff5ba505e6a
barkotenicholas <barkotenicholas@gmail.com>
2022-02-27 18:55:11 +0300

    Added try catch

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -32,6 +32,7 @@ public class App {
 
             String squadName = request.queryParams("squadName");
             String squadPurpose = request.queryParams("squadPurpose");
+
             int squadSize = Integer.parseInt(request.queryParams("size"));
             Squad squad = new Squad(squadName,squadSize,squadPurpose);
             model.put("squadName",squadName);
@@ -50,6 +51,8 @@ public class App {
 
         post("/hero/:id",(request, response) -> {
 
+            Map<String,Object> model = new HashMap<>();
+
             int id = Integer.parseInt(request.params(":id"));
             System.out.println("Id is"+id);
             String heroName = request.queryParams("heroName");
@@ -60,18 +63,27 @@ public class App {
             Hero hero = new Hero(heroName,age,heroStrength,heroWeakness);
             Squad squad = Squad.findById(id);
 
-            squad.addHeroes(hero);
-
-            Map<String,Object> model = new HashMap<>();
-            model.put("hero",heroName);
+            int squadSize =  squad.getSize();
+            int heroSize = squad.getMembers();
+            if(heroSize < squadSize){
+                squad.addHeroes(hero);
+                model.put("hero",heroName);
+            }else {
+                model.put("squad",squad.getName());
+            }
 
             return new ModelAndView(model,"herosuccess.hbs");
         },new HandlebarsTemplateEngine());
 
 
         get("/hero/new/:id",(request, response) -> {
-
-            int squadId = Integer.parseInt(request.params(":id"));
+            int squadId = 0;
+            try{
+                squadId = Integer.parseInt(request.params(":id"));
+            }
+            catch (NumberFormatException e){
+                e.printStackTrace();
+            }
             Map<String,Object> model = new HashMap<>();
             String squadName = Squad.findById(squadId).getName();
             model.put("squad",squadId);
@@ -82,7 +94,14 @@ public class App {
 
         get("/hero/:id",(request, response) -> {
 
-            int id = Integer.parseInt(request.params(":id"));
+
+            int id = 0;
+            try{
+                id = Integer.parseInt(request.params(":id"));
+            }
+            catch (NumberFormatException e){
+                e.printStackTrace();
+            }
             Squad one = Squad.findById(id);
             Map<String, Object> model = new HashMap<>();
             ArrayList<Hero> heroes = one.allHeroes();
